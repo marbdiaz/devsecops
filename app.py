0fb3b0fd5940d643bdbb72cf46935516fde29d80
marcelo diaz <marbdiaz@gmail.com>
2026-09-15 18:11:37 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def conectar_banco():
+    # ERRO DE SEGURANÇA 1: Credencial hardcoded no código fonte
+    password_db =os.getenv("DB_PASSWORD", "senha_padrao_local")
+    print(f"Conectando ao banco com a senha: {password_db}")
+
+def buscar_usuario(nome_usuario):
+    # ERRO DE SEGURANÇA 2: Concatenação direta de string (risco de SQL Injection)
+    conn = sqlite3.connect('banco_exemplo.db')
+    cursor = conn.cursor()
+    query = "SELECT * FROM usuarios WHERE nome = ?"
+    cursor.execute(query, (nome_usuario,))
+    return cursor.fetchall()
+
+if __name__ == "__main__":
+    conectar_banco()
