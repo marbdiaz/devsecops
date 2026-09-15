dea09f407020f1e8c8d6f1b8934becf7db490317
marcelo diaz <marbdiaz@gmail.com>
2026-09-15 18:16:25 -0300

    teste

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def conectar_banco():
     # ERRO DE SEGURANÇA 1: Credencial hardcoded no código fonte
-    password_db =os.getenv("DB_PASSWORD", "senha_padrao_local")
+    password_db = "senha_super_secreta"
     print(f"Conectando ao banco com a senha: {password_db}")
 
 def buscar_usuario(nome_usuario):
